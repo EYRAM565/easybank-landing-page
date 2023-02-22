7e60fe860eb4bdebdad00c4ac6b0acd3da2f5f1f
EYRAM565 <raymond.tublu@yahoo.com>
2023-02-22 19:34:28 +0000

    adding elements mobile version

--- style.js
+++ style.js
@@ -1,9 +1,17 @@
 const navTog = document.querySelector(".mobile_nav_toggle");
 const primnav = document.querySelector(".primary-navigation");
-const header = document.querySelector("header")
+const header = document.querySelector(".header")
 
 navTog.addEventListener('click', () =>{
-    primnav.hasAttribute('data-visible') ? navTog.setAttribute("aria-expanded", false) : navTog.setAttribute("aria-expanded", true);
-    primnav.toggleAttribute('data-visible');
+    const visibility = primnav.getAttribute('data-visible');
+    if(visibility === 'false'){
+        header.classList.add('wrapper-bgColor');
+        primnav.setAttribute('data-visible', true);
+        navTog.setAttribute('aria-expanded', true)
+    } else {
+        header.classList.remove('wrapper-bgColor');
+        primnav.setAttribute('data-visible', false);
+        navTog.setAttribute('aria-expanded', false);
+    }
     console.log('hello');
 })
